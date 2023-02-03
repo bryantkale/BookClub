80e21ac5dbe2967bd06fc6e37b00a84a910ad413
bryantcael <bryantca@grinnell.edu>
2023-02-02 21:16:00 -0500

    Adding tailwind to project

--- config/dev.exs
+++ config/dev.exs
@@ -26,7 +26,8 @@ config :book_club, BookClubWeb.Endpoint,
   secret_key_base: "7iaMsoHS8ZnRLj8FiLY22zCjnxROfvyaEr2yDwlofy62Vcbg2F8kop5E4ArRkOE3",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
